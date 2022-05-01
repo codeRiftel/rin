d64e38736dbd8f927ed968c4d7986f73f163d87b
coderiftel <coderiftel@gmail.com>
2022-05-02 01:27:14 +0300

    init

---
+++ Runtime/Rin.cs
@@ -0,0 +1,105 @@
+using System.Text;
+using System.Globalization;
+
+namespace rin {
+    public class Rin {
+        private StringBuilder builder;
+        private const string what = "\"\\/\b\f\n\r\t";
+
+        public Rin() {
+            builder = new StringBuilder();
+        }
+
+        public string Esc(string src) {
+            var bad = false;
+            for (int i = 0; i < what.Length; i++) {
+                bad = src.Contains(what[i]);
+                if (bad) break;
+            }
+
+            if (!bad) return src;
+
+            builder.Clear();
+            for (int i = 0; i < src.Length; i++) {
+                var escape = true;
+                var target = src[i];
+                switch (src[i]) {
+                    case '"':
+                    case '\\':
+                    case '/':
+                        break;
+                    case '\b':
+                        target = 'b';
+                        break;
+                    case '\f':
+                        target = 'f';
+                        break;
+                    case '\n':
+                        target = 'n';
+                        break;
+                    case '\r':
+                        target = 'r';
+                        break;
+                    case '\t':
+                        target = 't';
+                        break;
+                    default:
+                        escape = false;
+                        target = src[i];
+                        break;
+                }
+
+                if (escape) builder.Append('\\');
+                builder.Append(target);
+            }
+
+            return builder.ToString();
+        }
+
+        public static int? Int(string num) {
+            if (int.TryParse(num, out int res)) return res;
+            return null;
+        }
+
+        public static uint? Uint(string num) {
+            if (uint.TryParse(num, out uint res)) return res;
+            return null;
+        }
+
+        public static float? Float(string num) {
+            var style = NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent;
+            style |= NumberStyles.AllowLeadingSign;
+
+            if (float.TryParse(num, style, CultureInfo.InvariantCulture, out float r)) return r;
+            return null;
+        }
+
+        public static double? Double(string num) {
+            var style = NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent;
+            style |= NumberStyles.AllowLeadingSign;
+
+            if (double.TryParse(num, style, CultureInfo.InvariantCulture, out double r)) return r;
+            return null;
+        }
+
+        public static string To(float num) {
+            return num.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public static string To(double num) {
+            return num.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public static string To(int num) {
+            return num.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public static string To(uint num) {
+            return num.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public static string To(bool b) {
+            if (b) return "true"; else return "false";
+        }
+    }
+}
